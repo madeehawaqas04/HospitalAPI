8ba8c4902e8e71cc93e69750d7c9a1916b7f6cfa
madeehawaqas04 <124630346+madeehawaqas04@users.noreply.github.com>
2024-08-11 22:41:52 +0500

    Add project files.

---
+++ HospitalAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HospitalAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { }
+
+
+        public DbSet<Employee> Employees { get; set; } = null!;
+    }
+}
---
+++ HospitalAPI/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace HospitalAPI.Models
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
